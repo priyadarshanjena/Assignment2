9120492643e67a946a21ad5f09b9fd1210c1ca99
priyadarshanjena <59999152+priyadarshanjena@users.noreply.github.com>
2020-01-27 08:10:44 -0800

    Add files via upload

---
+++ Assignment2/Assignment2/EmployeeSalary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public abstract class Employee
+    {
+        public int EmpId;
+        public string EmpName;
+        public string Designation;
+        public int Exp;
+        public double AnnualSal;
+        public DateTime Date;
+
+        public abstract double CalculateSalary();
+    }
+
+    public class HR : Employee
+    {
+        private double basicsalary;
+
+
+        public override double CalculateSalary()
+        {
+            double basicsalary = 8000;
+            int Exp = 4;
+            return basicsalary * 1000 * (Exp);
+        }
+
+       
+    }
+    public class Developer : Employee
+    {
+        private double basicsalary1;
+
+        public override double CalculateSalary()
+        {
+            
+            
+                double basicsalary1 = 8000;
+                int Exp = 5;
+                return basicsalary1 * 2000 * Exp;
+            
+        }
+
+       
+    }
+}
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Assignment2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+   
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Employee emp = new HR();
+        Employee emp1 = new Developer();
+
+        Console.WriteLine("HR Salary :" + emp.CalculateSalary());
+        Console.WriteLine("Developer salary :"+ emp1.CalculateSalary());
+
+        Console.ReadKey();
+        }
+    }
+}
